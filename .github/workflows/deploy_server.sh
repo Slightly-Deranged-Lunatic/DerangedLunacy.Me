57c9829e7376a0a1feecfbe5ae202d16ff6b09f7
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-07-10 13:55:54 -0500

    Make deploy_server.sh actually deploy server

--- .github/workflows/deploy_server.sh
+++ .github/workflows/deploy_server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+    echo "Deploying Server"
 
-    echo "Deploying Server.."
+    cd /srv/webpages/DerangedLunacy.Me
+    git pull https://github.com/Slightly-Deranged-Lunatic/DerangedLunacy.Me
+
+    echo "Finished deploying"
