a112d7f508040ab10165b1fa153a2bf603c5a577
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-07-23 02:59:40 -0500

    blogs_list boilerplate

--- blog_helper/boilerplates.py
+++ blog_helper/boilerplates.py
@@ -34,6 +34,37 @@ def get_blog_post_boilerplate(dates):
 
     return boilerplate
 
+def get_blogs_list_boilerplates(dates):
+    month_name = dates["month_name"]
+    year = dates["year"]
+    boilerplate = f"""<!DOCTYPE html>
+
+<head>
+    <title>
+        {month_name} {year} Blogs
+    </title>
+    <style>
+        @import url("../../../../css/style.css");
+    </style>
+</head>
+
+<html>
+    <body>
+        <h1>
+            {month_name} {year} Blogs
+        </h1>
+        <li>
+
+        </li>
+        <footer>
+            <a href = "../list.html"> Go back to the {year} blogs </a> <br>
+            <a href = "../../blog_homepage.html"> Go back to all blogs </a> <br>
+            <a href = "../../../../index.html"> Go back to the homepage </a> <br>
+        </footer>
+    </body>
+</html>"""
+    return boilerplate
+
 def get_ordinal(number):
     # Automatically assign a numbers ordinal based off of the last digit
     SPECIAL_CASES = ["11", "12", "13"] # These numbers always use "th" despite ending in 1, 2, and 3, hence they're special cases
